0902c450b3f632b216f99e35ebc3e264c516365e
Jorge <jorge@pop-os.localdomain>
2022-12-05 17:00:48 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `JJ-home-solutions-frontend`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components"]
+};
